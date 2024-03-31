18dd974a42c6529ce021aba02530a0cea64ac0fc
takumakoike <takuma.151e@gmail.com>
2024-03-31 17:10:01 +0900

    env

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-
 import db from "@astrojs/db";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind(), db()]
+  integrations: [react(), tailwind(), db()],
+  output: "server",
+  adapter: vercel()
 });
